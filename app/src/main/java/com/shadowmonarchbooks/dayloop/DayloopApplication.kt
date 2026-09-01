3e05094e9859267b63172b7dcc501b615f52440f
shadowmonarchbooks-cloud <shadowmonarchbooks@gmail.com>
2026-09-01 19:31:31 +0800

    feat: keep launcher shortcut synced with active pack

--- app/src/main/java/com/shadowmonarchbooks/dayloop/DayloopApplication.kt
+++ app/src/main/java/com/shadowmonarchbooks/dayloop/DayloopApplication.kt
@@ -1,7 +1,16 @@
 package com.shadowmonarchbooks.dayloop
 
 import android.app.Application
+import com.shadowmonarchbooks.dayloop.launcher.LauncherShortcutUpdater
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class DayloopApplication : Application()
+class DayloopApplication : Application() {
+    @Inject lateinit var launcherShortcutUpdater: LauncherShortcutUpdater
+
+    override fun onCreate() {
+        super.onCreate()
+        launcherShortcutUpdater.start()
+    }
+}
