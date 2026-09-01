424e18cda53f394eb8fb6228ffe7f70a29049305
shadowmonarchbooks-cloud <shadowmonarchbooks@gmail.com>
2026-09-01 23:12:23 +0800

    Fix slash chrome weight import

--- app/src/main/java/com/shadowmonarchbooks/dayloop/ui/skin/SkinChrome.kt
+++ app/src/main/java/com/shadowmonarchbooks/dayloop/ui/skin/SkinChrome.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
